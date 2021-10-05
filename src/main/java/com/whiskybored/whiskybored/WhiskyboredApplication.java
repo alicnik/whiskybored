2071d5e93fabd6651711f0ce72a1aea6491edb6e
Alex Nicholas <alexander.nicholas@generalassemb.ly>
2021-10-05 11:05:08 +0100

    first commit

---
+++ src/main/java/com/whiskybored/whiskybored/WhiskyboredApplication.java
@@ -0,0 +1,13 @@
+package com.whiskybored.whiskybored;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiskyboredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiskyboredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whiskybored/whiskybored/WhiskyboredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whiskybored.whiskybored;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiskyboredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
